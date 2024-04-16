c42346e515e06cde50fa58704ed77cb24a861404
Inf0sth <joelaal15@outlook.com>
2024-04-16 08:35:45 -0600

    On branch proves Changes to be committed: 	new file:   Others/Ecuaciones_lineales.py

---
+++ Others/Ecuaciones_lineales.py
@@ -0,0 +1,18 @@
+# Resolución de ecucaciones lineales por Gaus Jordan
+from numpy import *
+
+matris_R = zeros((3,3))
+
+for i in range(3):
+        for j in range(3):
+            pos = i * 3 + j
+            if pos < 9:
+                numero = int(input(f"Ingrese el numero para {pos+1}: "))
+                matris_R[i, j] = numero
+            else:
+                break
+
+print("\n",matris_R)
+
+while matris_R[0][0] != 1 and matris_R[1][1] != 1 and matris_R[2][2] != 1:
+    pass

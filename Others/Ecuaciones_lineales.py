8e2020a227b6a782d5f43cc0a8016dec3133f18c
Inf0sth <joelaal15@outlook.com>
2024-04-22 17:35:53 -0600

    On branch proves Changes to be committed: 	modified:   Others/Ecuaciones_lineales.py

--- Others/Ecuaciones_lineales.py
+++ Others/Ecuaciones_lineales.py
@@ -2,7 +2,8 @@
 from numpy import *
 
 matris_R = zeros((3,3))
-
+# Matriz 1 = (x,y,z)
+# Matriz 2 = R1, R2,R3
 for i in range(3):
         for j in range(3):
             pos = i * 3 + j
@@ -16,3 +17,13 @@ print("\n",matris_R)
 
 while (matris_R[0][0],matris_R[1][1],matris_R[2][2]) != 1:
     pass
+
+"""
+while y[1][0] != 0:
+    if x[0][0] == -(y [1][0]):
+        x + y
+    elif x [0][0] == y [1][0]:
+        x - y
+    else:
+        x*y - y*x or x*y + y*x
+"""

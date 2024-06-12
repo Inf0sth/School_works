472969fedf6aacdf1b22435a431f740018531f5a
Inf0sth <joelaal15@outlook.com>
2024-06-12 07:39:09 -0600

    added: Exam 1

--- POO/Bank/src/Interfaces/AccountInterface.java
+++ POO/Bank/src/Interfaces/AccountInterface.java
@@ -1,17 +1,16 @@
+package Account;
 
-
-public interface AccountInterface{
-  public void createAccount(String name, String email, String phoneNumber, double initialBalance) throws AccountCreationException;
-  public String getAccountNumber();
-  public boolean getEmail(String email) throws AccountException;
-  public boolean setEmail(String email) throws AccountException;
-  public boolean getPhoneNumber(String phoneNumber) throws AccountException;
-  public boolean setPhoneNumber(String phoneNumber) throws AccountException;
-  public boolean getName(String name) throws AccountException;
-  public boolean setName(String name) throws AccountException;
-  public double getBalance();
-  public double withdraw(double amount) throws AccountException;
-  public double transfer(double amount, String concept) throws AccountException;
-  public double String consultTransaction(String Id) throws AccountException;
+public interface AccountInterface {
+    void createAccount(String name, String email, String phoneNumber, double initialBalance) throws AccountCreationException, AccountException;
+    String getAccountNumber();
+    boolean setEmail(String email) throws AccountException;
+    String getEmail() throws AccountException;
+    boolean setPhoneNumber(String phoneNumber) throws AccountException;
+    String getPhoneNumber() throws AccountException;
+    boolean setName(String name) throws AccountException;
+    String getName() throws AccountException;
+    double getBalance();
+    double withdraw(double amount) throws AccountException;
+    double transfer(double amount, String concept) throws AccountException;
+    String consultTransaction(String id) throws AccountException;
 }
-

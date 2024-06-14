9a46f5e0bd5f94e722acd5d76e5d08907520dcc0
Inf0 <106565371+Inf0sth@users.noreply.github.com>
2024-06-14 19:29:33 +0000

    On branch main Changes to be committed: 	new file:   C_A/Parcial3/05_V7FakeLinux/HEX/outDebug.exe 	new file:   C_A/Parcial3/05_V7FakeLinux/HEX/test_01.cpp

---
+++ C_A/Parcial3/05_V7FakeLinux/HEX/test_01.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    string strInput;
+    string key = "MVZMX-PVHA8-3YNHU-76AD8-YGGY5-NPWTH";
+    string key = "PVHA8-3YNHU-76AD8-YGGY5-NPWTH";
+    
+    cout << "Ingresa password_: ";
+    cin >> strInput;
+
+    if (strInput == key)
+    {
+        cout << "Completed!" << endl;
+    }
+    else
+    {
+        cout << "Failed" << endl;
+    }
+    
+    return 0;
+}
